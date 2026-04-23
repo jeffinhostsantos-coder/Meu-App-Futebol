66f8eef03f9428aa1b290a15144984d53d813aab
jeffinhostsantos-coder <jeffinhostsantos@gmail.com>
2026-04-23 09:51:32 -0300

    Update data source URL and restore functionality

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Analista Pro 2026", layout="wide")
 st.title("🎯 Analisador de Dados (GE & ESPN Edition)")
 
 # COLE O SEU LINK DO GOOGLE SHEETS (CSV) ABAIXO:
-URL_PLANILHA = "SUBSTITUA_PELO_SEU_LINK_AQUI"
+URL_PLANILHA = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR4oAQdRvnyDmqozRSX2wggU8ABqruw2LgD8P0mKqsLkYCe8CC14jeSKpZ6Q5IaAHjLKPlgdqXp0wPE/pub?output=csv"
 
 @st.cache_data(ttl=60)
 def carregar_dados():
